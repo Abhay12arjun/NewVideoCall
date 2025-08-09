58b2db9b3785e85df858bdf5232e6dc90100daf1
SinghAbhay <kr.abhaypca2019@gmail.com>
2025-08-09 12:24:28 +0530

    something changing

--- frontend/src/environment.js
+++ frontend/src/environment.js
@@ -1,8 +1,6 @@
+let IS_PROD = true;
+const server = IS_PROD ?
+    "https://apnavideobackend-fu8g.onrender.com" : "http://localhost:8000"
 
-// let IS_PROD=true;
 
-// const server=IS_PROD ?
-//     "https://videocall-huiq.onrender.com":
-//     "http://localhost:8000"
-
-// export default server;
+export default server;
